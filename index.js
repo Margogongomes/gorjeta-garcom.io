7c93b389f197834c1498f2ffe33fb322cc371f4e
Margo <margongomes@gmail.com>
2022-09-01 18:55:50 -0300

    commits atualizados

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const bodyParser = require('body-parser');
 
 const path = require("path");
 
 const jsonRoutes = require('./routes/json')
 const garcomRoutes = require('./routes/garcom')
+
+app.listen(port, ()=> {
+    console.log(`Running in http://localhost:${port}`);
+})
 
 app.use(express.json())
 
@@ -13,11 +18,17 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({extended: false}));
 
-app.listen(port, ()=> {
-    console.log(`Running in http://localhost:${port}`);
+app.post('/', (res, req)=>{
+
+    //valores que vão vir do forms
+    var email = req.body.email;
+    var senha = req.body.senha;
+
+    //array do erro
+    const erros = [];
 })
 
 app.use("/json", jsonRoutes)
-
 app.use("/",garcomRoutes)
